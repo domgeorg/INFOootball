986b2e17cd7b327bbac702304c6535048548df1a
domg <domgeorg@outlook.com.gr>
2017-11-16 19:55:06 +0200

    added resources

--- app/src/main/java/georgiopoulos/infootball/ui/Base/BaseFragment.java
+++ app/src/main/java/georgiopoulos/infootball/ui/Base/BaseFragment.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
@@ -63,6 +64,7 @@ public abstract class BaseFragment<P extends Presenter> extends NucleusSupportFr
     }
 
     public void newsFlash(final String message, final View viewId){
+        Log.e("-> MESSAGE <-",message);
         Snackbar.make(viewId,message,Snackbar.LENGTH_SHORT).show();
     }
 
---
+++ app/src/main/java/georgiopoulos/infootball/ui/Base/LCEFragment.java
@@ -0,0 +1,3 @@
+package georgiopoulos.infootball.ui.Base;
+
+public class LCEFragment{}
